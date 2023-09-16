7a117b56f359e2839572afce3ead5d31fa7424e2
Gabriel Azevedo <azevgabriel@gmail.com>
2023-09-16 16:06:55 -0300

    fix: wrong local import icons

--- client/src/components/Icons/index.ts
+++ client/src/components/Icons/index.ts
@@ -1,7 +1,6 @@
 import { BsFillGearFill } from '@react-icons/all-files/bs/BsFillGearFill';
 import { ImExit } from '@react-icons/all-files/im/ImExit';
+import { IoContrastOutline } from '@react-icons/all-files/io5/IoContrastOutline';
 import { TiArrowBack } from '@react-icons/all-files/ti/TiArrowBack';
 
-export { BsFillGearFill };
-export { ImExit };
-export { TiArrowBack };
+export { BsFillGearFill, ImExit, IoContrastOutline, TiArrowBack };
