9ec67539cb718c5be5e119346f276086cde1ca0f
Gabriel Azevedo <azevgabriel@gmail.com>
2023-09-22 22:07:43 -0300

    feat: add icons

--- client/src/components/Icons/index.ts
+++ client/src/components/Icons/index.ts
@@ -1,6 +1,21 @@
 import { BsFillGearFill } from '@react-icons/all-files/bs/BsFillGearFill';
+import { FaStopwatch } from '@react-icons/all-files/fa/FaStopwatch';
+import { FaTrophy } from '@react-icons/all-files/fa/FaTrophy';
+import { GiOnTarget } from '@react-icons/all-files/gi/GiOnTarget';
+import { GiTargetDummy } from '@react-icons/all-files/gi/GiTargetDummy';
 import { ImExit } from '@react-icons/all-files/im/ImExit';
 import { IoContrastOutline } from '@react-icons/all-files/io5/IoContrastOutline';
+import { IoTriangleSharp } from '@react-icons/all-files/io5/IoTriangleSharp';
 import { TiArrowBack } from '@react-icons/all-files/ti/TiArrowBack';
 
-export { BsFillGearFill, ImExit, IoContrastOutline, TiArrowBack };
+export {
+  BsFillGearFill,
+  FaStopwatch,
+  FaTrophy,
+  GiOnTarget,
+  GiTargetDummy,
+  ImExit,
+  IoContrastOutline,
+  IoTriangleSharp,
+  TiArrowBack,
+};
